468b04be6a7ad3f6bd052caca50019ccb50c8361
LfelipeCortesR <lfelipecr76@hotmail.com>
2022-04-22 14:51:08 -0500

    commit

--- app/services/UserServices.js
+++ app/services/UserServices.js
@@ -1,8 +1,16 @@
-const User= require('../models/user')
+const User= require('./../models/user')
 
 class UserServices {
-    static create(id,username,name,bio){
-        return new User(id,username,name,"sin bio")
+    static create(id,username,name){
+        return new User(id,username,name,"Sin bio")
+    }
+    static getInfo(user){
+        const info=[user.id,user.username,user.name,user.bio]
+        return info
+    }
+    static updateUserUsername(user,newUser){
+          user.username=newUser
+          return user
     }
 }
 
--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -1,3 +1,4 @@
+const User = require('../../app/models/User');
 const UserService = require('./../../app/services/UserServices')
 
 describe("Test for UserServices", () => {
@@ -8,5 +9,24 @@ describe("Test for UserServices", () => {
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
         });
-
+    test('2 Get all user data in a list ', () => {
+        const user = UserService.create(1,"LFelipe","Felipe")
+        const userInfoInList=UserService.getInfo(user)
+        expect(userInfoInList[0]).toBe(1) 
+        expect(userInfoInList[1]).toBe("LFelipe")
+        expect(userInfoInList[2]).toBe("Felipe")
+        expect(userInfoInList[3]).toBe("Sin bio")
+        });
+    test('3 Update username ', () => {
+        const user = UserService.create(1,"LFelipe","Felipe")
+        UserService.updateUserUsername(user, "connor")
+        expect(user.username).toBe("connor") 
+        });
+    test('4 Given a list of users give me the list of usernamer ', () => {
+        const user1 = UserService.create(1,"LFelipe","Felipe")
+        const usernames=UserService.getAllUsernames([user1,user2,user3])
+        expect(usernames).toContain("LFelipe1")
+        expect(usernames).toContain("LFelipe2")
+        expect(usernames).toContain("LFelipe3")
+        });
   })
