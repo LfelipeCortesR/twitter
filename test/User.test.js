a894f360e44e544618c85496191627293adcf50b
LfelipeCortesR <lfelipecr76@hotmail.com>
2022-04-22 12:58:31 -0500

    commit

--- test/User.test.js
+++ test/User.test.js
@@ -2,7 +2,7 @@ const User = require('./../app/models/User')
 
 describe("Unit Test for User class", () => {
     test('Create an user object ', () => {
-    const user = new User(1,"LFelipe","Felipe","Bio","dateCreated","lastUpdate")
+    const user = new User(1,"LFelipe","Felipe","Bio")
     expect(user.id).toBe(1)
     expect(user.username).toBe("LFelipe") 
     expect(user.name).toBe("Felipe")
